0770e61853469c924e639053b58ba03c9125423d
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2018-12-01 15:30:53 +0900

    truffle init

---
+++ truffle-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  networks: {
+    ganache: {
+      host: '127.0.0.1',
+      port: 7545,
+      network_id: 5777
+    }
+  },
+  mocha: {},
+  compilers: {
+    solc: {}
+  }
+}
