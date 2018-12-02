184f40929bf0b7485cc8b17b1e219a6bcad802e5
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2018-12-03 01:45:58 +0900

    add migration script

---
+++ migrations/2_deploy_reentrancy.js
@@ -0,0 +1,8 @@
+const Vulnerable = artifacts.require('./reentrancy/Vulnerable.sol');
+const Attacker   = artifacts.require('./reentrancy/Attacker.sol');
+
+module.exports = async (deployer) => {
+  await deployer.deploy(Vulnerable)
+  let vulnerable = await Vulnerable.deployed();
+  await deployer.deploy(Attacker, vulnerable.address);
+};
