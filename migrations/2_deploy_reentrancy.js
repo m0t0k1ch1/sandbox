5a66122e11802052eaa28dc76a0bd2423c9b8dd7
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2018-12-05 02:33:51 +0900

    fix deploy script

--- migrations/2_deploy_reentrancy.js
+++ migrations/2_deploy_reentrancy.js
@@ -2,7 +2,7 @@ const Vulnerable = artifacts.require('./reentrancy/Vulnerable.sol');
 const Attacker   = artifacts.require('./reentrancy/Attacker.sol');
 
 module.exports = async (deployer) => {
-  await deployer.deploy(Vulnerable)
+  await deployer.deploy(Vulnerable);
   let vulnerable = await Vulnerable.deployed();
   await deployer.deploy(Attacker, vulnerable.address);
 };
