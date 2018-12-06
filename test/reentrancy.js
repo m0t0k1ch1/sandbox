90ce23ea765379b7ee52e72c7f21653c0cb67866
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2018-12-06 11:58:19 +0900

    [reentrancy] use web3.utils.toWei()

--- test/reentrancy.js
+++ test/reentrancy.js
@@ -14,26 +14,26 @@ contract('Reentrancy', async (accounts) => {
     let reserveAmount;
 
     // Alice: deposit 1 ETH
-    await vulnerable.deposit({from: alice, value: 1000000000000000000});
+    await vulnerable.deposit({from: alice, value: web3.utils.toWei('1', 'ether')});
 
     balanceOfAlice = await vulnerable.balanceOf(alice);
-    assert.equal(1000000000000000000, balanceOfAlice);
+    assert.equal(web3.utils.toWei('1', 'ether'), balanceOfAlice);
     reserveAmount = await web3.eth.getBalance(vulnerable.address);
-    assert.equal(1000000000000000000, reserveAmount);
+    assert.equal(web3.utils.toWei('1', 'ether'), reserveAmount);
 
     // Bob: set up the attacker contract
     await attacker.setOwner(bob);
 
     for (var i = 0; i < 10; i++) {
       // Bob: deposit 0.1 ETH
-      await attacker.deposit({from: bob, value: 100000000000000000});
+      await attacker.deposit({from: bob, value: web3.utils.toWei('0.1', 'ether')});
 
       // Bob: withdraw and transfer 0.1 ETH
       await attacker.withdraw({from: bob});
     }
 
     balanceOfBob = await vulnerable.balanceOf(bob);
-    assert.equal(1000000000000000000, balanceOfBob);
+    assert.equal(web3.utils.toWei('1', 'ether'), balanceOfBob);
 
     // Bob: withdraw 1 ETH
     await vulnerable.withdraw({from: bob});
