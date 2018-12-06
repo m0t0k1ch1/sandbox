90127dc085fd807910768d93ce138f8c5a44f65d
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2018-12-06 12:40:17 +0900

    fix deploy script

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -6,7 +6,7 @@ const StorageManipulatable = artifacts.require('./StorageManipulatable.sol');
 module.exports = async (deployer) => {
   await deployer.deploy(Overflowable, 100, 100);
   await deployer.deploy(ReentrancyVulnerable);
-  let vulnerable = await ReentrancyVulnerable.deployed();
-  await deployer.deploy(ReentrancyAttacker, vulnerable.address);
+  let reentrancyVulnerable = await ReentrancyVulnerable.deployed();
+  await deployer.deploy(ReentrancyAttacker, reentrancyVulnerable.address);
   await deployer.deploy(StorageManipulatable, 10, 1000, 9999);
 };
