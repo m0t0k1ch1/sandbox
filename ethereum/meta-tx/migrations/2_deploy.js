ac3ae289796325ff79065a98785925e89321d6e8
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2019-03-09 03:53:01 +0900

    [meta-tx] fix migration scripts

--- ethereum/meta-tx/migrations/2_deploy.js
+++ ethereum/meta-tx/migrations/2_deploy.js
@@ -1,4 +1,4 @@
-const MetaToken = artifacts.require('./MetaToken.sol');
+const MetaToken = artifacts.require('MetaToken');
 
 module.exports = async (deployer) => {
   await deployer.deploy(MetaToken, web3.utils.toBN('21000000e18'), {
