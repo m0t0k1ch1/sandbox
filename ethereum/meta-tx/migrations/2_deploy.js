b8d54a0d8e28eed53f7d8f35c0357e96c1a3b8e5
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2019-03-09 14:34:32 +0900

    [meta-tx] fix total supply

--- ethereum/meta-tx/migrations/2_deploy.js
+++ ethereum/meta-tx/migrations/2_deploy.js
@@ -1,7 +1,9 @@
 const MetaToken = artifacts.require('MetaToken');
+const BN        = web3.utils.BN;
 
 module.exports = async (deployer) => {
-  await deployer.deploy(MetaToken, web3.utils.toBN('21000000e18'), {
-    gas: 2000000
+  let supply = new BN(21000000).mul(new BN(10).pow(new BN(18)));
+  await deployer.deploy(MetaToken, supply, {
+    gas: 3000000
   });
 };
