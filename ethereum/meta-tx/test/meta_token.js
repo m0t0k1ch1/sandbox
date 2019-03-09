9c19090a9abaf0e28a381f06873c31937a9bd15d
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2019-03-09 15:51:36 +0900

    [meta-tx] add test for metaTransfer

--- ethereum/meta-tx/test/meta_token.js
+++ ethereum/meta-tx/test/meta_token.js
@@ -20,4 +20,40 @@ contract('MetaToken', async (accounts) => {
     assert.equal(true, balanceOfFromBefore.sub(value).eq(balanceOfFromAfter));
     assert.equal(true, balanceOfToBefore.add(value).eq(balanceOfToAfter));
   });
+
+  it('metaTransfer', async () => {
+    let metaToken = await MetaToken.deployed();
+
+    let nonce         = new BN('0');
+    let gasPrice      = web3.utils.toWei(new BN('1'), 'gwei');
+    let gasLimit      = new BN('100000');
+    let tokenGasPrice = new BN('1');
+
+    let from    = accounts[0];
+    let to      = accounts[1];
+    let value   = new BN('1');
+    let params  = [nonce, gasPrice, gasLimit, tokenGasPrice];
+    let relayer = accounts[2];
+
+    let hash = await metaToken.metaTransferHash(from, to, value, params, relayer);
+    let sig  = await web3.eth.sign(hash, from);
+
+    let balanceOfFromBefore    = await metaToken.balanceOf(from);
+    let balanceOfToBefore      = await metaToken.balanceOf(to);
+    let balanceOfRelayerBefore = await metaToken.balanceOf(relayer);
+
+    await metaToken.metaTransfer(from, to, value, params, relayer, sig, {
+      from: relayer,
+      gasPrice: gasPrice
+    });
+
+    let balanceOfFromAfter    = await metaToken.balanceOf(from);
+    let balanceOfToAfter      = await metaToken.balanceOf(to);
+    let balanceOfRelayerAfter = await metaToken.balanceOf(relayer);
+
+    let fee = balanceOfRelayerAfter.sub(balanceOfRelayerBefore);
+
+    assert.equal(true, balanceOfFromBefore.sub(value).sub(fee).eq(balanceOfFromAfter));
+    assert.equal(true, balanceOfToBefore.add(value).eq(balanceOfToAfter));
+  });
 });
