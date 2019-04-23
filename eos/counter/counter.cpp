0c9deb6656007e418050eec519340d33774e8f46
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2019-04-24 02:20:40 +0900

    [eos] implement proxy target dapp

---
+++ eos/counter/counter.cpp
@@ -0,0 +1,21 @@
+#include "counter.hpp"
+
+using namespace eosio;
+
+void counter::increment(name me)
+{
+  require_auth(permission_level{me, REQUIRED_PERMISSION});
+
+  auto count_pk_itr = counts.find(me.value);
+  if (count_pk_itr == counts.end()) {
+    counts.emplace(_self, [&](auto& row) {
+      row.key = me;
+      row.val = 1;
+    });
+  }
+  else {
+    counts.modify(count_pk_itr, _self, [&](auto& row) {
+      row.val++;
+    });
+  }
+}
