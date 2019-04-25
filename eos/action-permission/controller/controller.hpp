c3eb3b4555bf4299b2be5b6c3b88cf1679a58184
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2019-04-25 14:55:29 +0900

    [eos/action-permission/counter] add authority.hpp

--- eos/action-permission/controller/controller.hpp
+++ eos/action-permission/controller/controller.hpp
@@ -1,35 +1,9 @@
 #pragma once
 
-#include <eosio/crypto.hpp>
 #include <eosio/eosio.hpp>
+#include "authority.hpp"
 
 using namespace eosio;
-
-struct key_weight
-{
-  public_key key;
-  uint16_t   weight;
-};
-
-struct permission_level_weight
-{
-  permission_level permission;
-  uint16_t         weight;
-};
-
-struct wait_weight
-{
-  uint32_t wait_sec;
-  uint16_t weight;
-};
-
-struct authority
-{
-  uint32_t                             threshold;
-  std::vector<key_weight>              keys;
-  std::vector<permission_level_weight> accounts;
-  std::vector<wait_weight>             waits;
-};
 
 class [[eosio::contract]] controller : public contract
 {
