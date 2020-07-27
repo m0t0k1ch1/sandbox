324ccfc662e4f628c798ed99eedd4b3686497f22
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2020-07-27 23:38:03 +0900

    [eos/account-factory] update factory contract

--- eos/account-factory/factory/factory.cpp
+++ eos/account-factory/factory/factory.cpp
@@ -3,18 +3,23 @@
 using namespace eosio;
 
 void factory::create(
-  const name&      account,
-  const authority& auth_owner,
-  const authority& auth_active,
-  uint32_t         ram_bytes
+  const name& account,
+  uint32_t    ram_bytes
 ) {
   require_auth(get_self());
+
+  authority auth = {
+    1,
+    {},
+    {permission_level_weight{{get_self(), name("eosio.code")}, 1}},
+    {}
+  };
 
   action(
     permission_level{get_self(), name("active")},
     name("eosio"),
     name("newaccount"),
-    std::make_tuple(get_self(), account, auth_owner, auth_active)
+    std::make_tuple(get_self(), account, auth, auth)
   ).send();
 
   action(
@@ -23,4 +28,55 @@ void factory::create(
     name("buyrambytes"),
     std::make_tuple(get_self(), account, ram_bytes)
   ).send();
+}
+
+void factory::init(
+  const name&      account,
+  const name&      code_key,
+  const authority& auth_owner,
+  const authority& auth_active
+) {
+  require_auth(get_self());
+
+  const auto& code = codes.get(code_key.value, "code is not found");
+
+  action(
+    permission_level{account, name("active")},
+    name("eosio"),
+    name("setcode"),
+    std::make_tuple(account, 0, 0, code.value)
+  ).send();
+
+  action(
+    permission_level{account, name("owner")},
+    name("eosio"),
+    name("updateauth"),
+    std::make_tuple(account, name("owner"), name(""), auth_owner)
+  ).send();
+
+  action(
+    permission_level{account, name("active")},
+    name("eosio"),
+    name("updateauth"),
+    std::make_tuple(account, name("active"), name("owner"), auth_active)
+  ).send();
+}
+
+void factory::setcode(
+  const name&              key,
+  const std::vector<char>& value
+) {
+  require_auth(get_self());
+
+  auto code_itr = codes.find(key.value);
+  if (code_itr == codes.end()) {
+    codes.emplace(get_self(), [&](auto& row) {
+      row.key   = key;
+      row.value = value;
+    });
+  } else {
+    codes.modify(code_itr, same_payer, [&](auto& row) {
+      row.value = value;
+    });
+  }
 }
--- eos/account-factory/factory/factory.hpp
+++ eos/account-factory/factory/factory.hpp
@@ -9,8 +9,27 @@ class [[eosio::contract]] factory : public contract
 {
   public:
 
+    factory(name receiver, name code, datastream<const char*> ds) : contract(receiver, code, ds), codes(get_self(), get_self().value) {}
+
     using contract::contract;
 
     [[eosio::action]]
-    void create(const name& account, const authority& auth_owner, const authority& auth_active, uint32_t ram_bytes);
+    void create(const name& account, uint32_t ram_bytes);
+
+    [[eosio::action]]
+    void init(const name& account, const name& code_key, const authority& auth_owner, const authority& auth_active);
+
+    [[eosio::action]]
+    void setcode(const name& key, const std::vector<char>& value);
+
+  private:
+
+    struct [[eosio::table]] code {
+      name              key;
+      std::vector<char> value;
+      uint64_t primary_key() const { return key.value; };
+    };
+    typedef eosio::multi_index<name("codes"), code> code_index;
+
+    code_index codes;
 };
